91786184a443938f2c040e989e0ef28220f63973
shubhamshinare <shubhamshinare2004@gmail.com>
2025-12-18 15:28:01 +0530

    Complete Home page component layout and sections

--- src/landing_page/home/Education.js
+++ src/landing_page/home/Education.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React from "react";
 
 function Education() {
-    return ( 
-        <h1> Education </h1>
-     );
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-6">
+          <img src="media/images/education.svg" style={{ width: "70%" }}></img>
+        </div>
+
+        <div className="col-6">
+          <h1 className="mb-3 fs-2">Free and open market education</h1>
+          <p>
+            Varsity, the largest online stock market education book in the world
+            covering everything from the basics to advanced trading.
+          </p>
+          <a href="" style={{ textDecoration: "none" }}>
+            Versity<i class="fas fa-angle-double-right"></i>
+          </a>
+          <p className="mt-5">
+            TradingQ&A, the most active trading and investment community in
+            India for all your market related queries.
+          </p>
+          <a href="" style={{ textDecoration: "none" }}>
+            TradingQ&A<i class="fas fa-angle-double-right"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Education;
