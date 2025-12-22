96891f033ab55baddc339ff8daff2613a0028ff2
shubhamshinare <shubhamshinare2004@gmail.com>
2025-12-22 16:40:11 +0530

    Connect MongoBD with Express

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3002;
+const uri = process.env.MONGO_URL;
 
 const app = express();
 
-app.listen(3002, () => {
+app.listen(PORT, () => {
   console.log("App Started!");
+  mongoose.connect(uri);
+  console.log("DB connected");
 });
