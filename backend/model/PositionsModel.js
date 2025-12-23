53aa8bcd52326e6177e539f6d6db20cb6ded1fd4
shubhamshinare <shubhamshinare2004@gmail.com>
2025-12-23 11:05:49 +0530

    Testing API Endpoints

--- backend/model/PositionsModel.js
+++ backend/model/PositionsModel.js
@@ -1,7 +1,7 @@
 const { model } = require("mongoose");
 
-const { PositionsModel } = require("../schemas/PositionsModel");
+const { PositionsSchema } = require("../schemas/PositionsSchema");
 
-const PositionsModel = new model("position", PositionsModel);
+const PositionsModel = new model("position", PositionsSchema);
 
 module.exports = { PositionsModel };
--- backend/schemas/HoldingsSchema.js
+++ backend/schemas/HoldingsSchema.js
@@ -1,4 +1,4 @@
-const { Schema } = required("mongoose");
+const { Schema } = require("mongoose");
 
 const HoldingsSchema = new Schema({
   name: String,
